d3423d47f6ddb0f2424603984eeaca414b350f45
Pierre Palatin <pierre@palatin.fr>
2014-08-30 14:58:09 -0700

    Some basic validity checks

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 mult = {};
 
 mult.MainCtrl = function($scope) {
-  this.foo = 'plop';
+  this.result = '';
+  this.resultPattern = /10/;
+  this.leftValue = 3;
+  this.rightValue = 7;
+  this.opSymbol = '+';
 };
 
 mult.module = angular.module('mult.app', []);
--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
     id: 'multapp',
     'bounds': {
-      'width': 400,
+      'width': 800,
       'height': 500
     }
   });
