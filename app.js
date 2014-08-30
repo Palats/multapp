29d15d59347c1ef70dd492f7444049855068333d
Pierre Palatin <pierre@palatin.fr>
2014-08-30 15:10:04 -0700

    Changing values

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 mult = {};
 
 mult.MainCtrl = function($scope) {
+  this.newValues();
+};
+
+mult.MainCtrl.prototype.randomValue = function() {
+  return Math.floor(Math.random() * 11); // 0 to 10, included
+};
+
+mult.MainCtrl.prototype.newValues = function() {
   this.result = '';
-  this.resultPattern = /10/;
-  this.leftValue = 3;
-  this.rightValue = 7;
+  this.leftValue = this.randomValue();
+  this.rightValue = this.randomValue();
+  this.expected = this.leftValue + this.rightValue;
   this.opSymbol = '+';
+  this.resultPattern = new RegExp(this.expected.toString());
 };
 
 mult.module = angular.module('mult.app', []);
